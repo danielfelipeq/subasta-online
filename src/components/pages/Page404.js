1345174b70d6e1c46e8eedf347f7350020df5057
danielfelipeq <danielquispe.m@gmail.com>
2022-05-27 19:10:06 -0500

    add image 404

--- src/components/pages/Page404.js
+++ src/components/pages/Page404.js
@@ -1,7 +1,18 @@
 import '../../assets/styles/Page404.scss'
+import Header from '../Layouts/Header'
 
 const Page404 = () => {
-  return <div>page-404</div>
+  return (
+    <div>
+      <Header />
+      <figure className="image-404">
+        <img
+          src="https://dam.muyinteresante.com.mx/wp-content/uploads/2020/04/error-404.jpg"
+          alt="404"
+        />
+      </figure>
+    </div>
+  )
 }
 
 export default Page404
